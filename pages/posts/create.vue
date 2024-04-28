acb0c5f6f45027e644287b875d62388e9d1d1cb5
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-04-28 13:57:50 +0600

    User ID dropdown works accordingly now.

--- pages/posts/create.vue
+++ pages/posts/create.vue
@@ -10,47 +10,43 @@
           <div class="border-b pb-12">
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
-
-
+              <!-- Cheap-ass select component. Should prolly overhaul to select instead of div with options later if possible. -->
               <div class="sm:col-span-3 lg:col-span-2">
                 <div class="relative w-full group">
-                  <!-- <label class="text-xs text-gray-400">Select Category</label> -->
-                  <!-- border-dimmed focus:border-brand -->
-                  <input class="hidden" value="{{ userId }}">
-                  <label class="block text-sm font-medium leading-6 text-zinc-800 dark:text-cyan-400">Select Category</label>
-                  <button @click.prevent 
-                    class="mt-2 py-2.5 px-3 w-full h-12 md:text-sm text-site bg-transparent 
-                      border-2 dark:border-slate-500 dark:focus:border-indigo-400
-                      focus:outline-none focus:ring-0 peer flex items-center justify-between rounded font-semibold"
-                      :class="{
-                        'dark:border-red-500 dark:focus:border-red-500': v$.userId.$error,
-                        'dark:border-slate-500 dark:focus:border-indigo-400': !v$.userId.$error,
-                      }"
+                  <input class="hidden" value="{{ createPostFormData.userId }}">
+                  <label class="block text-sm font-medium leading-6 text-zinc-800 dark:text-cyan-400"
                   >
-                    {{ createPostFormData.userId }}
+                    Select User
+                  </label>
+                  <button @click.prevent="dropdownOpened = true"
+                    v-on-click-outside="validateUserId"
+                    :class="{
+                      'text-gray-400' : createPostFormData.userId === '',
+                      'dark:border-red-500 dark:focus:border-red-500': v$.userId.$error,
+                      'dark:border-slate-500 dark:focus:border-indigo-400': !v$.userId.$error,
+                    }"
+                    class="mt-2 py-2.5 px-3 w-full h-12 md:text-sm text-site bg-transparent
+                      border-2 border-slate-500 dark:focus:border-indigo-400
+                      focus:outline-none focus:ring-0 peer flex items-center justify-between rounded font-semibold"
+                  >
+                    {{ createPostFormData.userId === "" ? "Select user" : createPostFormData.userId }}
                   </button>
                   <div class="absolute z-[99] top-[100%] left-[0%] translate-x-[0%] rounded-md overflow-hidden shadow-lg 
                               w-full peer-focus:visible peer-focus:opacity-100 opacity-0 invisible duration-200 p-1 
                               bg-gray-100 dark:bg-gray-800 border border-dimmed text-xs md:text-sm"
                   >
-                    <div class=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                      All (9)
-                    </div>
-                    <div class=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                      Full Stack (6)
-                    </div>
-                    <div class=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                      Front End (1)
-                    </div>
-                    <div class=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                      Freelance (1)
-                    </div>
-                    <div class=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
-                      New Stack
-                      Project (1)
+                    <div v-for="user in users.response.data" :key="user.key" @click="selectUser(user.id, user.name)" 
+                          class=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md"
+                    >
+                      {{ user.name }}
                     </div>
                   </div>
                 </div>
+                <span v-for="error of v$.userId.$errors" :key="error.$uid">
+                  <div class="text-red-500 mt-1 ml-2">
+                    {{ error.$message }}
+                  </div>
+                </span>
               </div>
 
               <div class="sm:col-span-3 lg:col-span-2">
@@ -70,14 +66,13 @@
                               shadow-lg text-2xl sm:text-sm sm:leading-6"
                   >
                 </div>
-                <span v-for="error of v$.$errors" :key="error.$uid">
-                  <div v-if="error.$property === 'title'" 
-                        class="text-red-500 mt-1 ml-2"
-                  >
+                <span v-for="error of v$.title.$errors" :key="error.$uid">
+                  <div class="text-red-500 mt-1 ml-2">
                     {{ error.$message }}
                   </div>
                 </span>
               </div>
+
               <div class="col-span-full">
                 <label for="text" class="block text-sm font-medium leading-6 text-slate-800 dark:text-cyan-400">text</label>
                 <div class="mt-2">
@@ -96,10 +91,8 @@
                           shadow-lg text-2xl sm:text-sm sm:leading-6"
                   ></textarea>
                 </div>
-                <span v-for="error of v$.$errors" :key="error.$uid">
-                  <div v-if="error.$property === 'text'" 
-                        class="text-red-500 mt-1 ml-2"
-                  >
+                <span v-for="error of v$.text.$errors" :key="error.$uid">
+                  <div class="text-red-500 mt-1 ml-2">
                     {{ error.$message }}
                   </div>
                 </span>
@@ -121,6 +114,12 @@
     <!-- End Create Post Form -->
     <h1 class="">
       {{ users }}
+      <br />
+      {{ createPostFormData }}
+      <br />
+      {{ createPostFormData.userId }}
+      <br />
+      {{ dropdownOpened }}
     </h1>
     <button class="px-3 py-1 border-2 border-cyan-400" @click="refresh">Refresh</button>
   </div>
@@ -129,6 +128,7 @@
 <script setup>
   import { required, helpers, email } from '@vuelidate/validators';
   import { useVuelidate } from '@vuelidate/core';
+  import { vOnClickOutside } from '@vueuse/components'
 
   definePageMeta({
     // NOTICE THAT THE LAYOUT COMPONENT IS NAMED MainLayout BUT HERE WE ARE CALLING IT main-layout. THIS IS BECAUSE WE CAN'T USE UNDERSCORES AND CAPITALS HERE AND ANY CAMELCASE IS PARSED AS 
@@ -141,25 +141,23 @@
   const currentPage = ref(1)
   const limit = ref(10)
   const search = ref('')
-  const userIdIsOpen = ref(false)
-  const userId = ref('')
 
   // Notice the arrow funciton as 1st param
-  const { data: users, pending, refresh, error, status } = await useFetch(() => `${runtimeConfig.public.API_URL}users?currentPage=${currentPage.value}&search=${search.value}&limit=${limit.value}`, {
-                                                                  onResponse({ request, response, options }) {
-                                                                    console.log('onResponseError', request, response, options);
-                                                                  },
-                                                                  onResponseError({ request, response, options }) {
-                                                                    // console.log('onResponseError', request, response, options);
-                                                                    errorStatus.value = response._data.response.error
-                                                                    errorMessage.value = response._data.response.message
-                                                                  }
-                                                                })
-
-  // userId.value = users
+  const { data: users, pending, refresh, error, status } = await useFetch(() => `${runtimeConfig.public.API_URL}users?currentPage=${currentPage.value}&search=${search.value}&limit=${limit.value}`
+                                                                // , {
+                                                                //   onResponse({ request, response, options }) {
+                                                                //     console.log('onResponseError', request, response, options);
+                                                                //   },
+                                                                //   onResponseError({ request, response, options }) {
+                                                                //     // console.log('onResponseError', request, response, options);
+                                                                //     errorStatus.value = response._data.response.error
+                                                                //     errorMessage.value = response._data.response.message
+                                                                //   }
+                                                                // }
+                                                              )
 
   const createPostFormData = reactive({
-    userId: 'Select User',
+    userId: '',
     title: '',
     text: '',
   });
@@ -184,7 +182,7 @@
 
   const v$ = useVuelidate(contactFormRules, createPostFormData)
 
-  const createPost =   () => {
+  const createPost = () => {
       v$.value.$validate();
       // console.log(v$.value.title,'values');
       console.log(v$.value.$errors,'errors');
@@ -194,6 +192,17 @@
         console.log('submit');
       }
   };
+
+  const selectUser = (id, name) => {
+    createPostFormData.userId = id
+  }
+
+  const dropdownOpened = ref(false)
+  const validateUserId = () => {
+    if(dropdownOpened.value){
+      v$.value.userId.$validate()
+    }
+  }
 
   // onMounted(async () => {
   //   try {
